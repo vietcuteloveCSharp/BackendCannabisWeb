07950a4daedbe3cdc89f30d61c17956358f1e3ac
vietcuteloveCSharp <vuvietanhsp@gmail.com>
2025-10-08 13:43:46 +0700

    DI

---
+++ Cannabis.Server/DependencyInjection/RepositoryExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Cannabis.Server.DependencyInjection
+{
+	public static class RepositoryExtensions
+	{
+		public static IServiceCollection AddApplicationRepositories(this IServiceCollection services)
+		{
+			services.AddScoped<IUnitOfWork, UnitOfWork>();
+			services.AddScoped<IAddressRepository, AddressRepository>();
+			services.AddScoped<IBrandRepository, BrandRepository>();
+			services.AddScoped<IBreederRepository, BreederRepository>();
+			services.AddScoped<ICarbonFilterRepository, CarbonFilterRepository>();
+			services.AddScoped<IChipModelRepository, ChipModelRepository>();
+			services.AddScoped<IClassificationRepository, ClassificationRepository>();
+			services.AddScoped<ICoolingSystemRepository, CoolingSystemRepository>();
+			services.AddScoped<IDehumidifierRepository, DehumidifierRepository>();
+			services.AddScoped<IGrowLightRepository, GrowLightRepository>();
+			services.AddScoped<IGrowTentRepository, GrowTentRepository>();
+			services.AddScoped<INutrientRepository, NutrientRepository>();
+			services.AddScoped<IProductRepository, ProductRepository>();
+			services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+			services.AddScoped<ISeedRepository, SeedRepository>();
+			services.AddScoped<IUserRepository, UserRepository>();
+			return services;
+		}
+	}
+}
---
+++ Cannabis.Server/DependencyInjection/ServiceExtenstions.cs
@@ -0,0 +1,30 @@
+﻿namespace Cannabis.Server.DependencyInjection{
+	public static class ServiceExtenstions
+	{
+		public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+		{
+			services.AddScoped<IAddressService, AddressService>();
+			services.AddScoped<IAuthService, AuthService>();
+			services.AddScoped<IBrandService, BrandService>();
+			services.AddScoped<IBreederService, BreederService>();
+			services.AddScoped<ICarbonFilterService, CarbonFilterService>();
+			services.AddScoped<IChipModelService, ChipModelService>();
+			services.AddScoped<IClassificationService, ClassificationService>();
+			services.AddScoped<ICoolingSystemService, CoolingSystemService>();
+			services.AddScoped<IGrowTentService, GrowTentService>();
+			services.AddScoped<INutrientService, NutrientService>();
+			services.AddScoped<INutrientTypeService, NutrientTypeService>();
+			services.AddScoped<IPowerSupplyService, PowerSupplyService>();
+			services.AddScoped<IRefreshTokenService, RefreshTokenService>();
+			services.AddScoped<IRoleService, RoleService>();
+			services.AddScoped<ISpectrumService, SpectrumService>();
+			services.AddScoped<ITokenService, TokenService>();
+			services.AddScoped<IUserService, UserService>();
+			services.AddSingleton<IRedisService, RedisService>();
+			services.AddScoped<IForgotPasswordService, ForgotPasswordService>();
+			services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+			services.AddScoped<IEmailService, EmailService>();
+			return services;
+		}
+	}
+}
