eea211495df8baa68f8ce0208400fc1c55ee3b9d
vietcuteloveCSharp <vuvietanhsp@gmail.com>
2025-10-14 02:32:31 +0700

    tao dto update

---
+++ DTO/DTOs/Users/UpdateUserDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace DTO.DTOs.Users
+{
+	public class UpdateUserDTO
+	{
+		public string Password { get; set; } = string.Empty;
+		public string? Name { get; set; }
+		public string Email { get; set; } = string.Empty;
+		public string PhoneNumber { get; set; } = string.Empty;
+		public EUserStatus Status { get; set; } = EUserStatus.Active;
+		public int RoleId { get; set; }
+	}
+}
