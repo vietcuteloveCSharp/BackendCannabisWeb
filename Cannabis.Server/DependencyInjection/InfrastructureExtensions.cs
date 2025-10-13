2862e2215cdb554db6dc930326fcd16ecb5d0f6d
vietcuteloveCSharp <vuvietanhsp@gmail.com>
2025-10-14 02:31:00 +0700

    tach cau hinh

---
+++ Cannabis.Server/DependencyInjection/InfrastructureExtensions.cs
@@ -0,0 +1,16 @@
+﻿using static Org.BouncyCastle.Math.EC.ECCurve;
+
+namespace Cannabis.Server.DependencyInjection
+{
+	public static class InfrastructureExtensions
+	{
+		public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration config)
+		{
+			services.Configure<MailSettings>(config.GetSection("Email"));
+			services.AddScoped<IEmailService, EmailService>();
+			services.AddSingleton<IRedisService, RedisService>();
+			services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+			return services;
+		}
+	}
+}
