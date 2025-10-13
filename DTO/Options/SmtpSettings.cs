3c4a762bb5bdcfe2272a8ef090c5ac8a5a952178
vietcuteloveCSharp <vuvietanhsp@gmail.com>
2025-10-14 02:34:31 +0700

    add option config

---
+++ DTO/Options/SmtpSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace DTO.Options
+{
+	public class SmtpSettings
+	{
+		public string Host { get; set; } = string.Empty;
+		public int Port { get; set; }
+		public string Username { get; set; } = string.Empty;
+		public string Password { get; set; } = string.Empty;
+		public bool UseSsl { get; set; }
+	}
+}
