fbbc0be0d3c87c219f13dca3b7f1ef06f544b96d
vietcuteloveCSharp <vuvietanhsp@gmail.com>
2025-10-14 02:33:09 +0700

    update map user

--- DTO/MapDTO_Entity/UserMappingProfile.cs
+++ DTO/MapDTO_Entity/UserMappingProfile.cs
@@ -11,7 +11,8 @@ namespace DTO.MapDTO_Entity
 				.ForMember(dest => dest.HashPassword, opt => opt.Ignore())
 				.ForMember(dest => dest.UpdatedAt, opt => opt.Ignore());
 			CreateMap<User, UserDTO>(MemberList.None)
-				.ForMember(dest => dest.Password, opt => opt.Ignore());		
+				.ForMember(dest => dest.Password, opt => opt.Ignore());
+			CreateMap<User, UpdateUserDTO>(MemberList.None);
 			CreateMap<User, UserSummaryDTO>(MemberList.None)
 			  .ForMember(dest => dest.RoleName, opt => opt.MapFrom(src => src.Role != null ? src.Role.RoleName.ToString() : null));
 			#endregion
