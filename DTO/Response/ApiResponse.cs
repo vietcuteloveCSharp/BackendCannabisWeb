f29f96983c8d565c0f6e7dfb9e7ac746576245b0
vietcuteloveCSharp <vuvietanhsp@gmail.com>
2025-10-20 17:14:07 +0700

    cau hinh response

---
+++ DTO/Response/ApiResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace DTO.Response
+{
+	public class ApiResponse<T>
+	{
+		public bool Success { get; set; }
+		public string Message { get; set; } = string.Empty;
+		public T? Data { get; set; }
+		public string? Error { get; set; }
+	
+
+		public static ApiResponse<T> Ok(T data, string message = "")
+			=> new() { Success = true, Message = message, Data = data };
+
+		public static ApiResponse<T> Fail(string message)
+			=> new() { Success = false, Message = message,Error=message };
+		public static ApiResponse<T> Content(string message) => new() { Success = true, Message = message};
+	}
+}
