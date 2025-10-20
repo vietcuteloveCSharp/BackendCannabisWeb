bde091fd4c5e19a8a0cbc2fb745f345bc74edfd8
vietcuteloveCSharp <vuvietanhsp@gmail.com>
2025-10-20 17:14:51 +0700

    add property ExpiresIn

--- DTO/Response/TokenResultDTO.cs
+++ DTO/Response/TokenResultDTO.cs
@@ -1,9 +1,12 @@
-﻿namespace DTO.Response
+﻿using DTO.DTOs.User.Users;
+
+namespace DTO.Response
 {
 	public class TokenResultDTO
 	{
 		public string AccessToken { get; set; } = default!;
 		public string RefreshToken { get; set; } = default!;
+		public int ExpiresIn { get; set; } // số giây còn lại
 		public UserSummaryDTO User { get; set; } = default!;
 	}
 }
