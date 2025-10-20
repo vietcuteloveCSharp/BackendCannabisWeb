41e602f213df332a7ab8218a49f39d28fed2d810
vietcuteloveCSharp <vuvietanhsp@gmail.com>
2025-10-20 17:34:04 +0700

    Remove deleted files

--- Service/IServices/IServicesAuth/IForgotPasswordService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Service.IServices.IServicesAuth
-{
-	public interface IForgotPasswordService
-	{
-		Task SendOtpAsync(string email);
-		Task ForgotPasswordAsync(ResetPasswordParam resetPasswordParam);
-	}
-}
--- Service/IServices/IServicesAuth/ITokenService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Service.IServices.IServicesAuth
-{
-	public interface ITokenService
-	{
-		string GenerateAccessToken(TokenPayload payload); // gen JWT access token
-		ClaimsPrincipal? ValidateToken(string token);
-		ClaimsPrincipal? GetPrincipalFromExpiredToken(string token);
-	}
-}
