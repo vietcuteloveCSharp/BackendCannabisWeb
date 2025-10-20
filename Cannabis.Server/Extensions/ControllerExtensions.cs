ac4c8779fd85926d3f90bed8736839e1dd4c95fb
vietcuteloveCSharp <vuvietanhsp@gmail.com>
2025-10-20 17:06:42 +0700

    extendsion

---
+++ Cannabis.Server/Extensions/ControllerExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Cannabis.Server.Extensions
+{
+	public static class ControllerExtensions
+	{
+		public static IActionResult ValidateModelState(this ControllerBase controller)
+		{
+			var errors = controller.ModelState
+				.Where(kvp => kvp.Value?.Errors.Count > 0)
+				.ToDictionary(
+					kvp => kvp.Key,
+					kvp => kvp.Value!.Errors.Select(e => e.ErrorMessage).ToArray()
+				);
+
+			var response = new ApiResponse<Dictionary<string, string[]>>
+			{
+				Success = false,
+				Message = "Validation failed",
+				Data = errors
+			};
+
+			return controller.BadRequest(response);
+		}
+	}
+}
