9859d8ecd53e5563814993062c5c2190da59a486
vietcuteloveCSharp <vuvietanhsp@gmail.com>
2025-10-25 17:49:58 +0700

    add dto service

--- Cannabis.Server/Program.cs
+++ Cannabis.Server/Program.cs
@@ -38,7 +38,7 @@
 				});
 
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-			builder.Services.AddEndpointsApiExplorer();
+				 builder.Services.AddEndpointsApiExplorer();
 				 builder.Services.AddSwaggerGen();
 				 builder.Services.AddDbContext<CannabisAccessorriesDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("CannabisAccessorriesDB")));
 
