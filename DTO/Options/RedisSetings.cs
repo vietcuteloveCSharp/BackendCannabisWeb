e4320248b0542cc846ccc284f7c06a5abac53801
vietcuteloveCSharp <vuvietanhsp@gmail.com>
2025-10-25 17:27:39 +0700

    redis option

---
+++ DTO/Options/RedisSetings.cs
@@ -0,0 +1,9 @@
+﻿namespace DTO.Options
+{
+	public class RedisSetings
+	{
+		public string Host { get; set; } = null!;
+		public int Port { get; set; } = 6379;
+		public string? Password { get; set; } // nếu có password
+	}
+}
