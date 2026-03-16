7b6205de73617fe0a7724ec32bb60725ba554f28
vietcuteloveCSharp <vuvietanhsp@gmail.com>
2026-03-17 04:12:31 +0700

    refactor: sua code DTO

--- DTO/Request/LoginResquestDTO.cs
+++ DTO/Request/LoginResquestDTO.cs
@@ -8,7 +8,9 @@ namespace DTO.Request
 {
 	public class LoginResquestDTO
 	{
+		[Required(ErrorMessage = "Username is required")]
 		public string Username { get; set; } = string.Empty;
+		[Required(ErrorMessage = "Password is required")]
 		public string Password { get; set; } = string.Empty;
 	}
 }
