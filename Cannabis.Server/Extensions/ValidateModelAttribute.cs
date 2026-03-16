3884e49339f61d255165e1d1014f4468371416c3
vietcuteloveCSharp <vuvietanhsp@gmail.com>
2026-03-17 04:03:13 +0700

    style: Sua loi chinh ta, chinh cau hinh cho gon

---
+++ Cannabis.Server/Extensions/ValidateModelAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cannabis.Server.Extensions
+{
+	public class ValidateModelAttribute: ActionFilterAttribute
+	{
+		public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			if (!context.ModelState.IsValid)
+			{
+				var errors = context.ModelState
+					.Where(x => x.Value.Errors.Count > 0)
+					.SelectMany(x => x.Value.Errors)
+					.Select(e => e.ErrorMessage)
+					.ToArray();
+
+				throw new ValidationException(string.Join("; ", errors)); // ném exception
+			}
+		}
+	}
+}
