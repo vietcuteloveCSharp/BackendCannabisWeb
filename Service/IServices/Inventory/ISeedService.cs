a072043adef1a8012341d0228cc0e6ccd7fafd7e
vietcuteloveCSharp <vuvietanhsp@gmail.com>
2026-03-17 21:49:44 +0700

    refactor: sua logic seed vs product

---
+++ Service/IServices/Inventory/ISeedService.cs
@@ -0,0 +1,14 @@
+﻿using DTO.DTOs.Seeds;
+
+namespace Service.IServices.Inventory
+{
+	public interface ISeedService
+	{
+		Task<IEnumerable<SeedDTO>> GetAllAsync();
+		Task<IEnumerable<SeedDTO>> GetAllActiveAsync();
+		Task<SeedDTO?> GetByIdAsync(int id);
+		Task<SeedDTO> CreateAsync(SeedCreateRequestDTO dto);
+		Task<bool> UpdateAsync(int id, SeedUpdateDTO dto);
+		Task<bool> DeleteAsync(int id);
+	}
+}
