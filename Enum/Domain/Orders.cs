e6493f59291a5473ed4d14ce48134fb470e49241
vietcuteloveCSharp <vuvietanhsp@gmail.com>
2026-03-22 17:38:02 +0700

    refactor: tách enum

---
+++ Enum/Domain/Orders.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum.Domain
+{
+	public static class Orders
+	{
+
+		public enum EOrderStatus
+		{
+			Pending = 0,
+			Confirmed = 1,
+			Processing = 2,
+			Shipped = 3,
+			Delivered = 4,
+			Canceled = 5,
+			Returned = 6,
+			Failed = 7
+		}
+
+		public enum ECartStatus
+		{
+			Active = 0,
+			CheckedOut = 1,
+			Abandoned = 2 // User thêm hàng nhưng 1 tuần không mua
+		}
+
+		public enum EDiscountType
+		{
+			Percent = 0, // Giảm %
+			Fixed = 1    // Giảm số tiền cụ thể
+		}
+	}
+}
---
+++ Enum/Domain/System_User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum.Domain
+{
+	public static class System_User
+	{
+		public enum ERoleName
+		{
+			Admin = 1,
+			Employee = 3,
+			User = 2
+		}
+		public enum EUserStatus
+		{
+			Active = 0,
+			Inactive = 1,
+			Suspended = 2 // Dùng để test case: User bị khóa không được login
+		}
+	}
+}
